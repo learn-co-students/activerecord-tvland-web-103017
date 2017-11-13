1b7971abbe264b5ea2b893f175e01f5902a8c30f
jjwumaster <jjwumaster@gmail.com>
2017-11-13 14:09:56 -0500

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "Steve Urkel always says: #{self.catchphrase}"
+  end
+
+  def build_show(hash)
+    # binding.pry
+    self.show = Show.new(hash)
+  end
+
+  def build_network(hash)
+    self.show.network = Network.new(hash)
+  end
+
 end
